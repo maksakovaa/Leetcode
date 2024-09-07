441706d9b77a0b65c59083a3df1d44f1eba6958d
Akeksandr Maksakov <a.makskakov@mail.ru>
2024-09-07 12:31:04 +0300

    Best Time to Buy and Sell Stock added

---
+++ 121_BestTimeToBuyAndSellStock.cpp
@@ -0,0 +1,90 @@
+/*
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+Example 1:
+
+Input: prices = [7,1,5,3,6,4]
+Output: 5
+Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+
+Example 2:
+
+Input: prices = [7,6,4,3,1]
+Output: 0
+Explanation: In this case, no transactions are done and the max profit = 0.
+
+*/
+
+#include <vector>
+#include <algorithm>
+#include <iostream>
+using std::vector;
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices)
+    {
+        int profit = 0;
+        int min{0};
+        for (int i = 1; i < prices.size(); i++)
+        {
+            if (prices[i] < prices[min])
+            {
+                min = i;
+            }
+
+            if (prices[i] - prices[min] > profit)
+            {
+                profit = prices[i] - prices[min];
+            }
+        }
+        return profit;
+    }
+};
+
+void prinVec(std::vector<int> nums)
+{
+    std::cout << "Result is: ";
+    for (int i: nums)
+    {
+        std::cout << i << ", ";
+    }
+    std::cout << "\n";
+}
+void checkResult(int out, int result)
+{
+    if (out == result)
+    {
+        std::cout << "Test passed!" << std::endl;
+    }
+    else
+    {
+        std::cout << "Test failed! Result is: " << out << std::endl;
+    }
+}
+
+
+void testCase1()
+{
+    Solution s;
+    vector<int> nums = {7,1,5,3,6,4};
+    int output = 5;
+    checkResult(s.maxProfit(nums), output);
+}
+
+void testCase2()
+{
+    Solution s;
+    vector<int> nums = {7,6,4,3,1};
+    int output = 0;
+    checkResult(s.maxProfit(nums), output);
+}
+
+int main()
+{
+    testCase1();
+    testCase2();
+}
--- README.md
+++ README.md
@@ -3,5 +3,6 @@ Solutions for leetcode tasks:
 - [27. Remove Element](https://github.com/maksakovaa/Leetcode/blob/main/027_RemoveElement.cpp)
 - [80. Remove Duplicates from Sorted Array II](https://github.com/maksakovaa/Leetcode/blob/main/080_RemoveDuplicatesFromSortedArrayII.cpp)
 - [88. Merge Sorted Array](https://github.com/maksakovaa/Leetcode/blob/main/088_MergeSortedArray.cpp)
+- [121. Best Time to Buy and Sell Stock](https://github.com/maksakovaa/Leetcode/blob/main/121_BestTimeToBuyAndSellStock.cpp)
 - [169. Majority Element](https://github.com/maksakovaa/Leetcode/blob/main/169_MajorityElement.cpp)
 - [189. Rotate Array](https://github.com/maksakovaa/Leetcode/blob/main/189_RotateArray.cpp)
